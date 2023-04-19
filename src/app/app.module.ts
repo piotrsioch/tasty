b1159f5c640993320207ad7d385a1273dc09f3ff
Rafał Zan <01159225@pw.edu.pl>
2023-04-19 21:26:33 +0200

    Feature/add login register page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { CoreModule } from "./core/core.module";
 import { LoginModule } from "./modules/login/login.module";
 import { AppRoutingModule } from "./app-routing.module";
 import { DashboardModule } from "./modules/dashboard/dashboard.module";
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -16,6 +17,7 @@ import { DashboardModule } from "./modules/dashboard/dashboard.module";
     LoginModule,
     DashboardModule,
     AppRoutingModule,
+    BrowserAnimationsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/modules/login/login.module.ts
+++ src/app/modules/login/login.module.ts
@@ -1,6 +1,8 @@
 import { NgModule } from "@angular/core";
 import { LoginComponent } from "./login.component";
 import { RouterModule, Routes } from "@angular/router";
+import {NgClass, NgForOf} from "@angular/common";
+import { AuthFormComponent } from './auth-form/auth-form.component';
 
 const routes: Routes = [{
   path: '',
@@ -10,9 +12,12 @@ const routes: Routes = [{
 @NgModule({
   declarations: [
     LoginComponent,
+    AuthFormComponent,
   ],
   imports: [
     RouterModule.forChild(routes),
+    NgClass,
+    NgForOf,
   ],
   exports: [
     RouterModule,
